8095299e7b2bdefa76269545fd23dca0a7a85fd9
kesavardhankakani <kesavardhankakani@email.com>
2026-07-14 17:22:17 +0530

    project done

--- static/js/app.js
+++ static/js/app.js
@@ -307,3 +307,26 @@ window.onload=function(){
         getProjectProgress();
     }
 };
+function togglePassword(){
+
+    const password=document.getElementById("password");
+    const icon=document.getElementById("togglePassword");
+
+    if(password.type==="password"){
+
+        password.type="text";
+
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+
+    }
+    else{
+
+        password.type="password";
+
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+
+    }
+
+}
